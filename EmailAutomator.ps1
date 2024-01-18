5450de23c9d3d2cea989216d1b5dfb1eb73bc896
Nicholas Cabrera <ncabrera528@gmail.com>
2024-01-18 14:27:43 -0500

    uploading the script
    before running the script, please ensure it has not been compromised and is still safe to use.

---
+++ EmailAutomator.ps1
@@ -0,0 +1,25 @@
+# EmailAutomator.ps1
+
+Param (
+    [Parameter(Mandatory, HelpMessage = "Please provide the name of the caller.")]
+    [string]$CallerName,
+    [Parameter(Mandatory, HelpMessage = "Please provide the prefix of the caller, ie. Mr. or Ms.")]
+    [string]$CallerPrefix,
+    [Parameter(Mandatory, HelpMessage = "Please provide the reason the caller is calling.")]
+    [string]$Reference,
+    [Parameter(Mandatory, HelpMessage = "Please provide the phone number for the caller.")]
+    [string]$CallerNumber,
+    [Parameter(Mandatory, HelpMessage = "Please provide the calls recipient.")]
+    [string]$Name
+)
+
+Write-Host `"$CallerPrefix`"
+
+If (-not (($CallerPrefix -eq 'Mr.') -or ($CallerPrefix -eq 'Ms.') -or ($CallerPrefix -eq 'Mrs.'))){
+    Write-Host "Please enter a valid caller prefix."
+} Else {
+    Write-Host "Hi $Name,"
+    Write-Host "$CallerPrefix $CallerName called today in reference to $Reference and may be contacted at $CallerNumber."
+    Write-Host "V/R"
+    Write-Host "Nico"
+}
